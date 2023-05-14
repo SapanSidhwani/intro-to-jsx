5eccb0e717f5c63a09ffd2c2630f94de768d60a2
Sapan <sapansidhwani40@gmail.com>
2023-05-14 20:39:04 +0530

    Javascript ES6 Import, Export and Modules Practice

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import './App.css';
 import React from 'react';
-
-import PI, { doublePI, triplePI } from './Math';
-/*
-
-another way of using above line code  
-
-import * as pi from './Math';
-
----- How to use it ------
-pi.PI()
-pi.doublePI()
-pi.triplePI()
-
-*/
-
+import PI, { add, multiply, subtract, divide } from './Math';
 import Heading from './components/Heading';
 
 function App() {
-    return (
-      <>
-        <Heading />
-        <ul>
-          <li>{PI()}</li>
-          <li>{doublePI()}</li>
-          <li>{triplePI()}</li>
-        </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <Heading />
+      <ul>
+        <li> {PI()} </li>
+        <li> {add(15, 3)} </li>
+        <li> {multiply(15, 3)} </li>
+        <li> {subtract(15, 3)} </li>
+        <li> {divide(15, 3)} </li>
+      </ul>
+    </>
+  );
+}
 
 export default App;
--- src/Math.js
+++ src/Math.js
@@ -1,16 +1,22 @@
-const PI = 3.141529; 
+const PI = 3.141529;
 
 const Math = () => {
-  return ( PI );
-}
-
-const doublePI = () => {
-    return ( PI * 2 );
-}
-
-const triplePI = () => {
-    return (PI * 3);
-}
+    return (PI);
+};
 
 export default Math;
-export { doublePI, triplePI};
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+
+export { add, multiply, subtract, divide };
