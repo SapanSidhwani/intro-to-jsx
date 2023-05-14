9b52ba8828edb3565ba94da3b6f1017490281de8
Sapan <sapansidhwani40@gmail.com>
2023-05-14 19:31:59 +0530

    React Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
-
-var hour = new Date().getHours();
-var greeting = "";
-var customStyle = {
-  color: ""
-};
-
-if(hour < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-}
-else if(hour < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-}
-else {
-  greeting = "Good Evening";
-  customStyle.color = "blue";
-}
+import Heading from './components/Heading';
+import List from './components/List';
 
 function App() {
   return (
     <>
-      <h1 className='heading' style={customStyle}>{greeting}</h1>
+      <Heading/>
+      <List/>
     </>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <>
+      <ul>
+        <li>Aloo Paratha</li>
+        <li>Maggi</li>
+        <li>Dal Pakwan</li>
+      </ul>
+    </>
+  )
+}
+
+export default List;
