f7baafbe18b2f6161fb7c0b26ee89a2ac640a2de
Sapan <sapansidhwani40@gmail.com>
2023-05-14 18:45:30 +0530

    Inline Styling for React Elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 
-var image = "https://picsum.photos/200/100";
+var customStyle = {
+  color: "red",
+  fontSize: "24px",
+  border: "5px solid black"
+};
+
+customStyle.color = " blue";
 
 function App() {
   return (
     <>
-      <h1 className='heading' contentEditable="true" spellCheck="false">My favourite images</h1>
-      <div>
-        <img className='adventure-img' src={image} alt="lorem images" />
-        <img className='adventure-img' src={image + "?grayscale"} alt="lorem images" />
-        <img className='adventure-img' src={image + "?blur"} alt="lorem images" />
-      </div>
+      <h1 style={customStyle}>Hello World</h1>
     </>
   );
 }
