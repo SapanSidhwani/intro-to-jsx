e43c93f83bde0dcda19d8b8d43f010d86237f659
Sapan <sapansidhwani40@gmail.com>
2023-05-14 16:41:11 +0530

    js expressions and ES6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+const name = "sapan";
+const number = "3";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello {name}</h1>
+      <h2>Your Lucky Number is {Math.floor(Math.random() * 10) + 1}</h2>
       <ul>
         <li>Paneer</li>
         <li>Noodles</li>
