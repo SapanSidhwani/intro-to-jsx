82f3cfbcfedbbc64820b737bf142c49908908a72
Sapan <sapansidhwani40@gmail.com>
2023-05-14 19:16:13 +0530

    React Styling Practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 
+var hour = new Date().getHours();
+var greeting = "";
 var customStyle = {
-  color: "red",
-  fontSize: "24px",
-  border: "5px solid black"
+  color: ""
 };
 
-customStyle.color = " blue";
+if(hour < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+}
+else if(hour < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+}
+else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
 
 function App() {
   return (
     <>
-      <h1 style={customStyle}>Hello World</h1>
+      <h1 className='heading' style={customStyle}>{greeting}</h1>
     </>
   );
 }
