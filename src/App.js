e17d15639513e3081a8bac79cbbd67a19878c999
Sapan <sapansidhwani40@gmail.com>
2023-05-14 20:30:02 +0530

    Javascript ES6 - Import, Export and Modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import React from 'react';
+
+import PI, { doublePI, triplePI } from './Math';
+/*
+
+another way of using above line code  
+
+import * as pi from './Math';
+
+---- How to use it ------
+pi.PI()
+pi.doublePI()
+pi.triplePI()
+
+*/
+
 import Heading from './components/Heading';
 
 function App() {
-  return (
-    <>
-      <Heading/>
-    </>
-  );
-}
+    return (
+      <>
+        <Heading />
+        <ul>
+          <li>{PI()}</li>
+          <li>{doublePI()}</li>
+          <li>{triplePI()}</li>
+        </ul>
+      </>
+    );
+  }
 
 export default App;
---
+++ src/Math.js
@@ -0,0 +1,16 @@
+const PI = 3.141529; 
+
+const Math = () => {
+  return ( PI );
+}
+
+const doublePI = () => {
+    return ( PI * 2 );
+}
+
+const triplePI = () => {
+    return (PI * 3);
+}
+
+export default Math;
+export { doublePI, triplePI};
